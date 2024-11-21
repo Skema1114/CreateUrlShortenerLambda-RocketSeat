aa3a8d3f9b629965d637b3082db1dba9067f4155
Rafael Aranda Martins <skema1114@hotmail.com>
2024-11-21 11:21:53 -0300

    feat(aula 02): finalizada a aula 02;

--- src/main/java/com/rocketseat/createUrlShortenerLambda/Main.java
+++ src/main/java/com/rocketseat/createUrlShortenerLambda/Main.java
@@ -3,6 +3,9 @@ package com.rocketseat.createUrlShortenerLambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +14,7 @@ import java.util.UUID;
 public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
 	private final ObjectMapper objectMapper = new ObjectMapper();
+	private final S3Client s3Client = S3Client.builder().build();
 
 	@Override
 	public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
@@ -26,7 +30,24 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Obj
 
 		String originalUrl = bodyMap.get("originalUrl");
 		String expirationTime = bodyMap.get("expirationTime");
+		long expirationTimeInSeconds = Long.parseLong(expirationTime);
+
 		String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+		UrlData urlData = new UrlData(originalUrl, expirationTimeInSeconds);
+
+		try {
+			String urlDataJson = objectMapper.writeValueAsString(urlData);
+
+			PutObjectRequest requestObj = PutObjectRequest.builder()
+					.bucket("url-shortener-storage-rocketseat-lambda-bucket")
+					.key(shortUrlCode + ".json")
+					.build();
+
+			s3Client.putObject(requestObj, RequestBody.fromString(urlDataJson));
+		} catch (Exception exception) {
+			throw new RuntimeException("Error saving data to S3: " + exception.getMessage(), exception);
+		}
 
 		Map<String, Object> response = new HashMap<>();
 		response.put("code", shortUrlCode);
